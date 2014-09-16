0bd64e3668d5edd59ff04b3088382d040f850796
techytimo <techytimo@blackpearl.localdomain>
2014-09-17 00:33:44 +0300

    partners display hack

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require modernizr
 //= require jquery-ui
 // require jquery.dropotron
 // require jquery.popover_menu
@@ -325,5 +326,33 @@ $(document).ready(function(){
 	//   // }
 	// });
 
+	// image hover overlay effect
+    if (Modernizr.touch) {
+        // show the close overlay button
+        $(".close-overlay").removeClass("hidden");
+        // handle the adding of hover class when clicked
+        $(".partners li").click(function(e){
+            if (!$(this).hasClass("hover")) {
+                $(this).addClass("hover");
+            }
+        });
+        // handle the closing of the overlay
+        $(".close-overlay").click(function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            if ($(this).closest(".partners li").hasClass("hover")) {
+                $(this).closest(".partners li").removeClass("hover");
+            }
+        });
+    } else {
+        // handle the mouseenter functionality
+        $(".partners li").mouseenter(function(){
+            $(this).addClass("hover");
+        })
+        // handle the mouseleave functionality
+        .mouseleave(function(){
+            $(this).removeClass("hover");
+        });
+    }
 
 });  
